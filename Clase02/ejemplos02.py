1d3b9aace3f88c0600d182e1bdbd1ea890a3e037
Fernando <77fernandoroldan77@gmail.com>
2025-05-01 17:13:06 -0300

    agregada la clase02

---
+++ Clase02/ejemplos02.py
@@ -0,0 +1,35 @@
+## EJEMPLOS PARA SALTO DE LINEA
+
+# Nueva Línea (\n)
+
+# Inserta un salto de línea en el texto impreso.
+
+print("Hola\nMundo")
+
+# Salida por consola: 
+# Hola
+# Mundo
+
+
+# Tabulación (\t)
+
+# Añade un conjunto fijo de espacios en blanco.
+
+print("Nombre:\tJuan\nEdad:\t30")
+
+# Salida por consola:
+
+# Nombre: Juan
+
+# Edad: 30
+
+# End
+
+# Añade una especificación que se incorpora al final de la cadena
+
+print("Hola,", end=' ')
+print("Mundo")
+
+# Salida por consola:
+
+# Hola, Mundo
